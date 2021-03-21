ed505a5126460e4f6ec2cf090663c57c5144a136
contact@kenada.fr <contact@kenada.fr>
2021-03-21 18:30:18 +0100

    Test Heroku

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;                         // Permet d'utiliser les routes sans "config/routes.yaml"
+use App\Repository\UserRepository;                                      // Class UserRepository
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class DefaultController extends AbstractController
+
+{
+
+    /**
+
+     * @Route("/home", name="default_home")
+
+     */
+    public function home(UserRepository $userRepository)
+
+    {
+/*
+        $user = new User();
+        $user->setIdUser(10);
+        $user->setLastName("test");
+        $user->setFirstName("kenada");
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+*/
+
+        return $this->render('index.html.twig', [
+                        'title' => 'Ma page de contact ',
+                        'test' => $userRepository->findAll()
+                    ]);
+
+    }
+    /**
+
+     * @Route("/posts/{id}")
+
+     */
+    public function post(int $id)
+
+    {
+
+        return $this->render('index.html.twig', [
+
+            'title' => 'Ma page de contact'. $id,
+
+        ]);
+
+    }
+
+}
