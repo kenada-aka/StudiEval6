70e4064e8ccdaa4c0211beace18c6a09a2ad38a9
contact@kenada.fr <contact@kenada.fr>
2021-04-24 00:53:45 +0200

    ChoiceSelect Pays

--- src/Form/StashType.php
+++ src/Form/StashType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Stash;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -16,7 +17,13 @@ class StashType extends AbstractType
         $builder
             ->add('code', TextType::class)
             ->add('adress', TextType::class)
-            ->add('country', TextType::class)
+            ->add('country', ChoiceType::class, [
+                'choices'  => [  
+                    "France" => "France",
+                    "Espagne" => "Espagne",
+                    "Maroc" => "Maroc",
+                    "Belgique" => "Belgique"
+                ]])
             ->add('type', TextType::class)
             //->add('idMission')
         ;
